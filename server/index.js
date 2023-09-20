cfd50a7643175cc49c810c0e6b57262ba7ec0fe5
dukekunyura <dukekunyura@gmail.com>
2023-09-21 01:00:17 +0300

    [+] add controllers router and models

--- server/index.js
+++ server/index.js
@@ -3,13 +3,21 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const { default: mongoose } = require('mongoose');
+const router = require('./router/index');
 
 const PORT = process.env.PORT || 5000;
+const URL = process.env.BD_URL;
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
 
 const start = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL, {
+        await mongoose.connect(URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })

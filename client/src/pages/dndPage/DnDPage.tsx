6301bbd8d08fdb0d2053c90799db52c1d7be73ab
dukekunyura <dukekunyura@gmail.com>
2023-10-17 17:02:33 +0300

    [+] add checkerLight and appendChild

--- client/src/pages/dndPage/DnDPage.tsx
+++ client/src/pages/dndPage/DnDPage.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { DetailedHTMLProps, FC, HTMLAttributes, useEffect } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import DnDCard from "../../components/DnDCard";
 
@@ -7,12 +7,13 @@ const DnDPage: FC = () => {
 
     const columnStyle = {
         "display": "flex",
-        "flexDirection": "column",
         "margin": "6px",
         "border": "1px solid",
         "borderColor": "gray",
         "backgroundColor": "whitesmoke",
-        "alignItems": "center"
+        "alignItems": "center",
+        "flexDirection": "column"
+
     };
 
     const arr = [1, 7, 6, 7, 8, 1, 5, 4];
@@ -30,13 +31,63 @@ const DnDPage: FC = () => {
         return newArr.filter(el => el !== null);
     };
 
-    const result = checker(arr);
+    const checkerHard = (arr: number[]) => {
+        const uniqueElements: { [key: number]: boolean } = {};
+        const result: number[] = [];
+
+        for (const num of arr) {
+            if (!uniqueElements[num]) {
+                uniqueElements[num] = true;
+                result.push(num);
+            }
+        }
+
+        return result;
+    };
+
+    const checkerLight = (arr: number[]) => {
+        const counts: { [key: number]: number } = {};
+        const result: number[] = [];
+
+        for (const num of arr) {
+            counts[num] = (counts[num] || 0) + 1;
+        }
+
+        for (const num of arr) {
+            if (counts[num] === 1) {
+                result.push(num);
+            }
+        }
+
+        return result;
+    };
+
+    const result = checkerLight(arr);
 
     console.log("result: " + result);
+
+    type TUuu = string | number | null;
+
+    const uuu: TUuu = "5";
+
+    const testFun = (uuu: number) => {
+        console.log(uuu);
+    }
+
+    testFun(uuu as unknown as number);
+
+    useEffect(() => {
+        const list = document.querySelector("h3");
+        const el = document.createElement("a");
+        el.textContent = "DnD test";
+        list?.appendChild(el);
+
+    }, [])
 
     return (
         <>
             <div className="container">
+                <h3></h3>
                 <br />
                 <div className="row align-items-start">
                     <div className="col" style={columnStyle}>
